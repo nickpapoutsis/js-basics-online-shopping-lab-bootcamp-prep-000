3e5d66190a7067f097098796810ca62c4d7b20f1
Nick Papoutsis <izybit@gmail.com>
2017-11-23 16:59:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,30 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
-}
+ let obj = { [item]: Math.floor(Math.random() * 99 + 1) };
+ cart.push(obj);
+ console.log(`${item} has been added to your cart.`);
+ return cart;
+}/*
+function addToCart(item) {
+ cart[item] = Math.floor(Math.random() * 99 + 1);
+ console.log(`${item} has been added to your cart.`);
+ return cart;
+}*/
 
 function viewCart() {
-  // write your code here
+  if (cart.length == 0) {
+    console.log(`Your shopping cart is empty.`)
+  }
+  if (cart.length == 1) {
+    console.log(`In your cart, you have ${Object.keys(cart[0])}.`)
+  }
+  if (cart.length > 1) {
+    var string = ""
+    for (var i = 0; i < cart.length - 1; i++) {
+      cart[i]
+    }
+  }
 }
 
 function total() {
